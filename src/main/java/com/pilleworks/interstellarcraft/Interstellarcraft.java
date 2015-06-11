cf251467fe8b83eb9eb1f47c4a474bc04584854f
Pille <cython@web.de>
2015-06-11 14:23:53 +0200

    Initial Forge project commit.

---
+++ src/main/java/com/pilleworks/interstellarcraft/Interstellarcraft.java
@@ -0,0 +1,8 @@
+package com.pilleworks.interstellarcraft;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = "Interstellarcraft", name = "Interstellarcraft", version = "1.8-0.0.1")
+public class Interstellarcraft {
+
+}
